04f7ff1a96acb6b6ca44d1fff5ff71dfa561dce5
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-10 08:49:08 +0530

    profile section added

---
+++ client/src/components/specific/Profile.jsx
@@ -0,0 +1,59 @@
+import { Avatar,Stack, Typography } from '@mui/material'
+import React from 'react'
+import{
+  Face as FaceIcon,
+  AlternateEmail as UserNameIcon,
+  CalendarMonth as CalendarIcon
+} from '@mui/icons-material'
+import moment from "moment"
+
+const Profile = () => {
+  return (
+    <Stack 
+      spacing={"2rem"}
+      direction={"column"}
+      alignItems={"center"}
+     > 
+      <Avatar
+         sx={{
+          width: 200,
+          height: 200,
+          objectFit: "contain",
+          marginBottom: "1rem",
+          border: "5px solid white",
+         }}
+      />
+      <ProfileCard heading={"Bio"} text={"Hello,How are you?"} />
+      <ProfileCard heading={"Username"} text={"k.ranjann"} Icon={<UserNameIcon />} />
+      <ProfileCard heading={"Name"} text={"Ranjan Kumar"} Icon={<FaceIcon />} />
+      <ProfileCard heading={"Joined"} text={moment("2024-07-10T00:00:00.000Z").fromNow()} Icon={<CalendarIcon />} />
+
+
+
+    </Stack>
+  )
+}
+
+const ProfileCard=({text,Icon,heading}) =>(
+  <Stack
+     direction={"row"}
+     alignItems={"center"}
+     spacing={"1rem"}
+     color={"white"}
+     textAlign={"center"}
+    >
+     {
+      Icon && Icon
+     }
+     <Stack>
+      <Typography variant='body1'>{text}</Typography>
+      <Typography color={"gray"} variant='caption'>
+        {heading}
+      </Typography>
+
+
+     </Stack>
+  </Stack>
+) 
+
+export default Profile
