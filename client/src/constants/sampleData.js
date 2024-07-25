6fee9c5a7951369f5950f556e90cffe5ad2baa7f
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-25 16:28:06 +0530

    Search page created

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -6,13 +6,26 @@
       groupChat: false,
       members:["1","2"],
     },
+
     {
-      avatar: [
-        "https://www.w3schools.com/howto/img_avatar.png"
-      ],
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
       name: "Ranjan Shah",
       _id: "2",
       groupChat: true,
       members:["1","2"],
     },
   ]
+
+  export const sampleUsers= [
+    {
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      name: "Virat Kohli",
+      _id: "1",
+    },
+    {
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      name: "Ranjan Shah",
+      _id: "2",
+    },
+  ]
+
