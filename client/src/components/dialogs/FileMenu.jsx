48b979006c63d9198944618431c6e73a128ad8e8
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-27 11:43:00 +0530

    Home and chat components added

---
+++ client/src/components/dialogs/FileMenu.jsx
@@ -0,0 +1,20 @@
+import { Menu } from '@mui/material'
+import React from 'react'
+
+const FileMenu = ({anchorE1}) => {
+  return (
+    <Menu anchorEl={anchorE1}>
+      <div
+        style={{
+          width:"10rem",
+        }}
+      >
+        ranjaj jasoh kjsggi kjiusokla jihyuasjhuihj uhasjhihbsh,
+        jshahj hsuhuhsalahsuhiasjjsuhjos sijijsaojxuhs jsh suhijs 
+        hugsuhi hs
+      </div>
+    </Menu>
+  )
+}
+
+export default FileMenu
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { Fragment,containerRef } from 'react'
 import AppLayout from '../components/layout/AppLayout'
+import { IconButton, Stack } from '@mui/material';
+import { grayColor, orange } from '../constants/color';
+import { AttachFile as AttachFileIcon, Send as SendIcon } from '@mui/icons-material';
+import { InputBox } from '../components/styles/StyledComponents';
+import FileMenu from '../components/dialogs/FileMenu';
 
 const Chat = () => {
   return (
-    <div>
-      Chat 
-    </div>
+    <Fragment>
+      <Stack 
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflowX: "hidden",
+          overflowY: "auto",
+        }}
+      >
+        {/* messages render */}
+      </Stack>
+      <form
+        style={{
+          height:"10%",
+        }}
+      >
+
+        <Stack
+         direction={"row"}
+         height={"100%"}
+         padding={"1rem"}
+         alignItems={"center"}
+         position={"relative"}
+        >
+          <IconButton
+             sx={{
+              position:"absolute",
+              left: "1.5rem",
+              rotate: "30deg",
+             }}
+          >
+            <AttachFileIcon />
+          </IconButton>
+
+          <InputBox placeholder='Type Message Here...' />
+
+          <IconButton
+             type='submit'
+             sx={{
+              rotate:"-30deg",
+              bgcolor: orange,
+              color: "white",
+              marginLeft: "1rem",
+              padding: "0.5rem",
+              "&:hover":{
+                bgcolor:"error.dark",
+              }
+             }}
+          >
+             <SendIcon />
+          </IconButton>
+
+        </Stack>
+
+      </form>
+      <FileMenu />
+    </Fragment>
   )
 }
 
