1bf9c7d27f6b8b60e80842c09e2c9afb015445dd
Ranjan kumar <ranjankumar5673@gmail.com>
2024-08-06 16:44:11 +0530

    dashboard completed

--- client/src/components/styles/StyledComponents.jsx
+++ client/src/components/styles/StyledComponents.jsx
@@ -1,6 +1,6 @@
 import { styled } from "@mui/material";
 import { Link as LinkComponent } from "react-router-dom";
-import { grayColor } from "../../constants/color";
+import { grayColor, matBlack } from "../../constants/color";
 
 
 export const VisuallyHiddenInput=styled('input')({
@@ -42,4 +42,18 @@ export const SearchField = styled("input")`
   border-radius: 1.5rem;
   background-color: ${grayColor};
   font-size: 1.1rem;
+`;
+
+export const CurveButton =styled("button")`
+  border-radius: 1.5rem;
+  padding: 1rem 2rem;
+  border:  none;
+  outline: none;
+  cursor: pointer;
+  background-color: ${matBlack};
+  color: white;
+  font-size: 1.1rem;
+  &:hover{
+   background-color:rgba(0,0,0,0.8)
+   }
 `;
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -3,9 +3,14 @@ import AdminLayout from '../../components/layout/AdminLayout'
 import { Box, Button, Container, Input, Paper, Stack, Typography } from '@mui/material'
 import {
    AdminPanelSettings as AdminPanelSettingsIcon,
+   Group as GroupIcon,
+   Message as MessageIcon,
+   Notifications as NotificationsIcon,
+   Person as PersonIcon,
   } from '@mui/icons-material'
 import moment from 'moment'
-import { SearchField } from '../../components/styles/StyledComponents'
+import { CurveButton, SearchField } from '../../components/styles/StyledComponents'
+import { matBlack } from '../../constants/color'
 
 const Dashboard = () => {
 
@@ -24,20 +29,137 @@ const Dashboard = () => {
        spacing={"1rem"}
       >
         <AdminPanelSettingsIcon sx={{fontSize:"3rem"}} />
-        <SearchField />
-        <button>sdf</button>
+        <SearchField placeholder='Search...' />
+        <CurveButton>Search</CurveButton>
         <Box flexGrow={1} />
-        <Typography>{moment().format("MMMM Do YYYY")}</Typography>
+        <Typography
+          display={{
+            xs: "none",
+            lg: "block",
+          }}
+          color={"rgba(0,0,0,0.7)"}
+          textAlign={"center"}
+        >
+          {moment().format("dddd, D MMMM YYYY")}
+        </Typography>
+        <NotificationsIcon />
       </Stack>
     </Paper>
+  );
+  const Widgets=(
+    <Stack
+      direction={{
+        xs: "column",
+        sm:"row",
+      }}
+      spacing={"2rem"}
+      justifyContent="space-between"
+      alignItems={"center"}
+      margin={"2rem 0"}
+    >
+      <Widget title={"Users"} value={34} Icon={<PersonIcon/>} />
+      <Widget title={"Chats"} value={4} Icon={<GroupIcon/>} />
+      <Widget title={"Messages"} value={454} Icon={<MessageIcon/>} />
+
+    </Stack>
   )
   return (
     <AdminLayout>
       <Container component={"main"}>
         {Appbar}
+        <Stack
+          direction={"row"}
+          spacing={"2rem"}
+          flexWrap={"wrap"}
+        >
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "2rem 3.5rem",
+              borderRadius: "1rem",
+              width: "100%",
+              maxWidth: "45rem",
+              height: "25rem"
+            }}
+          >
+            <Typography margin={"2rem 0"} variant='h4'>
+              Last Messages
+            </Typography>
+            {"Chat"}
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "1rem",
+              borderRadius: "1rem",
+              display: 'flex',
+              justifyContent: "center",
+              alignItems: "center",
+              width:{xs:"100%", sm: "50%"},
+              position: "relative",
+              width: "100%",
+              maxWidth: "25rem",
+              height: "25rem"
+            }}
+          > 
+          { " Dougnut Chart"}
+
+          <Stack
+            position={"absolute"}
+            direction={"row"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            spacing={"0.5rem"}
+            width={"100%"}
+            height={"100%"}
+          >
+            <GroupIcon />
+            <Typography>Vs</Typography>
+            <PersonIcon />
+
+          </Stack>
+
+          </Paper>
+        </Stack>
+        {
+          Widgets
+        }
       </Container>
     </AdminLayout>
   )
 }
+
+const Widget=({title,value,Icon})=>(
+  <Paper
+    elevation={3}
+    sx={{
+      padding: "2rem",
+      margin: "2rem 0",
+      borderRadius: "1.5rem",
+      width: "20rem",
+    }}
+  >
+    <Stack alignItems={"center"} spacing={"1rem"}>
+      <Typography
+        sx={{
+          color: "rgba(0,0,0,0.7)",
+          borderRadius: "50%",
+          border: `5px solid ${matBlack}`,
+          width: "5rem",
+          height: "5rem",
+          display: "flex",
+          justifyContent:"center",
+          alignItems:"center",
+        }}
+      >
+        {value}
+      </Typography>
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        {Icon}
+        <Typography>{title}</Typography>
+      </Stack>
+    </Stack>
+  </Paper>
+)
 
 export default Dashboard
