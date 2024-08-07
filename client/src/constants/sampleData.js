0462ec03afa1885cefd78c652ae898dc7a320b6c
Ranjan kumar <ranjankumar5673@gmail.com>
2024-08-07 23:53:38 +0530

    created table for chats and messages

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -97,5 +97,87 @@
         groups: 5,
       },
     ],
+
+    chats: [
+      {
+        name: "chad coders",
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        _id: "1",
+        groupChat: false,
+        members:[
+          {
+            _id:"1",
+            avatar: "https://www.w3schools.com/howto/img_avatar.png",
+          },
+          {
+            _id:"2",
+            avatar: "https://www.w3schools.com/howto/img_avatar.png",
+          },
+        ],
+        totalMembers:2,
+        totalMessages:20,
+        creator: {
+          name:"Virat Kohli",
+          avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        },
+      },
+
+      {
+        name: "knock knock",
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        _id: "2",
+        groupChat: true,
+        members:[
+          {
+            _id:"1",
+            avatar: "https://www.w3schools.com/howto/img_avatar.png",
+          },
+          {
+            _id:"2",
+            avatar: "https://www.w3schools.com/howto/img_avatar.png",
+          },
+        ],
+        totalMembers:2,
+        totalMessages:20,
+        creator: {
+          name:"Ranjan Shah",
+          avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        },
+      },
+    ],
+
+    messages:[
+      {
+        attachments:[],
+        content:"Hello,how r u?",
+        _id: "shgskhghsdgk",
+        sender:{
+          avatar: "https://www.w3schools.com/howto/img_avatar.png",
+          name:" Ranjan ",
+        },
+        chat: "chatId",
+        groupChat:false,
+        createdAt:"2024-08-07T12:34:56.789Z",
+      },
+
+      {
+        attachments:[
+          {
+            public_id: "Ranjan 2",
+            url: "https://www.w3schools.com/howto/img_avatar.png",
+
+          }
+        ],
+        content:"Hii,I m good.what abt u ?",
+        _id: "shgskhghsdgkmk",
+        sender:{
+          avatar: "https://www.w3schools.com/howto/img_avatar.png",
+          name:" Ranjan 2 ",
+        },
+        chat: "chatId",
+        groupChat:true,
+        createdAt:"2024-08-07T12:34:56.789Z",
+      },
+    ],
   }
 
