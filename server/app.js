46311eaae45bedf9e50e71f861c6e43ace562ff2
Ranjan kumar <ranjankumar5673@gmail.com>
2024-08-12 19:45:02 +0530

    error handling methods added

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoute from "./routes/user.route.js"
 import {connectDB}  from "./utils/features.js";
 import dotenv from "dotenv";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 dotenv.config({
   path:"./.env",
@@ -22,6 +23,8 @@ app.use('/user',userRoute);
 app.get("/",(req,res)=>{
   res.send("Hello World");
 });
+
+app.use(errorMiddleware);
 
 
 app.listen(port,()=>{
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,10 @@
 import { compare } from "bcrypt";
 import {User} from "../models/user.model.js"
 import { sendToken } from "../utils/features.js";
+import { TryCatch } from "../middlewares/error.middleware.js";
+import { ErrorHandler } from "../utils/utility.js";
+
+
 const newUser = async(req,res)=>{
   const {name,username,password,bio} = req.body;
   const avatar={
@@ -17,16 +21,19 @@ const newUser = async(req,res)=>{
   sendToken(res,user,201,"User created");
 };
 
-const login = async(req,res)=>{
+const login = TryCatch( async(req,res,next)=>{
   const {username,password} = req.body;
   const user=await User.findOne({username}).select("+password");
-  if(!user)return res.status(400).json({message: "Invalid username"});
+  if(!user)
+    return next(new ErrorHandler("Invalid Username or Password",404));
 
   const isMatch = await compare(password,user.password);
-  if(!isMatch)return res.status(400).json({message: "Invalid password"});
-  sendToken(res,user,200,`Welcome Back, ${user.name}`);
+  if(!isMatch)
+    return next(new ErrorHandler("Invalid Username or Password",404));
+   sendToken(res,user,200,`Welcome Back, ${user.name}`);
 
+});
 
-};
+const getMyProfile = async(req,res)=>{};
 
-export{ login,newUser }
+export{ login,newUser,getMyProfile }
