cda9445d21971a0eeef5b2a38780ccce31a792f3
Ranjan kumar <ranjankumar5673@gmail.com>
2024-08-22 19:56:23 +0530

    renameGroup api created

--- server/controllers/chat.controller.js
+++ server/controllers/chat.controller.js
@@ -275,7 +275,28 @@ const getChatDetails = TryCatch(async(req,res,next)=>{
       chat,
     });
   }
-})
+});
+
+const renameGroup = TryCatch(async(req,res,next)=>{
+  const chatId = req.params.id;
+  const {name} = req.body;
+
+  const chat = await Chat.findById(chatId);
+  if(!chat) return next(new ErrorHandler("Chat not found",404));
+  if(!chat.groupChat) return next(new ErrorHandler("This is not a group chat",400));
+  if(chat.creator.toString()!==req.user.toString())
+    return next (
+        new ErrorHandler("You are not allowed to rename the group",403)
+  );
+  chat.name = name;
+  await chat.save();
+  emitEvent(req,REFETCH_CHATS,chat.members);
+
+  return res.status(200).json({
+    success: true,
+    message: "Group renamed successfully",
+  });
+});
 
 
 export {
@@ -287,4 +308,5 @@ export {
   leaveGroup,
   sendAttachments,
   getChatDetails,
+  renameGroup,
 };
