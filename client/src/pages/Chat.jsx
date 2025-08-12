81dfa14f63bcec1ac8afd4158f4898d9b9507a18
Ranjan kumar <ranjankumar5673@gmail.com>
2025-08-12 12:53:20 +0530

    fix chat

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -7,7 +7,7 @@ import { InputBox } from '../components/styles/StyledComponents';
 import FileMenu from '../components/dialogs/FileMenu';
 import MessageComponent from '../components/shared/MessageComponent';
 import { getSocket } from '../socket';
-import { NEW_MESSAGE, START_TYPING, STOP_TYPING } from '../constants/events';
+import { ALERT, NEW_MESSAGE, START_TYPING, STOP_TYPING } from '../constants/events';
 import { useChatDetailsQuery, useGetMessagesQuery } from '../redux/api/api';
 import { useErrors, useSocketEvents } from '../hooks/hook';
 import { useInfiniteScrollTop } from '6pp';
@@ -114,7 +114,21 @@ const Chat = ({chatId,user}) => {
 
   },[chatId]);
 
+  const alertListener = useCallback((content)=>{
+    const messageForAlert = {
+          content,
+          sender:{
+            _id: "kdkjsjgeuhwuhd",
+            name: "Admin",
+          },
+          chat: chatId,
+          createdAt: new Date().toISOString(),
+        };
+        setMessages((prev)=>[...prev, messageForAlert]);
+  },[chatId]);
+
   const eventHandler = {
+    [ALERT] : alertListener,
     [NEW_MESSAGE]:newMessageListener,
     [START_TYPING]:startTypingListener,
     [STOP_TYPING]: stopTypingListener,
